10996c215213a8d8fa738b5d62166219b96473c7
kanu Chukwuemeka <mekuseo@gmail.com>
2024-10-11 10:28:52 +0100

    link to mongoose DB

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -76,7 +76,7 @@ const PropertyCard = ({ property }) => {
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="flex align-middle gap-2 mb-4 lg:mb-0">
           <FaMapMarker className="text-orange-700 mt-1"/>
-            <span className="text-orange-700"> {property.location.city}{property.location.state}</span>
+            <span className="text-orange-700"> {property.location.city} {property.location.state}</span>
           </div>
           <Link
             href={`/properties/${property._id}`}
